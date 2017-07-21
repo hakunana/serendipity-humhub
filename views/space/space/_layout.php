295f725939edb1ebf63139e7e0292b5e55b8200a
SRMonti <stevemonti_@hotmail.com>
2017-07-21 15:57:17 +0200

    Sidenav adding user list
    Added user list in spacelayout php

--- views/space/space/_layout.php
+++ views/space/space/_layout.php
@@ -74,10 +74,11 @@ $space = $this->context->contentContainer;
     </div>
     <div class="sidenav_content_hidden" style="display: none">
         <?php
-        echo \humhub\modules\space\widgets\Sidebar::widget(['space' => $this->context->contentContainer,
-            'widgets' => [[\humhub\modules\activity\widgets\Stream::className(),
-                ['streamAction' => '/space/space/stream','contentContainer' => $this->context->contentContainer],
-                ['sortOrder' => 10]]]]);
+        echo \humhub\modules\space\widgets\Sidebar::widget(['space' => $space, 'widgets' => [
+            [\humhub\modules\activity\widgets\Stream::className(), ['streamAction' => '/space/space/stream', 'contentContainer' => $space], ['sortOrder' => 10]],
+            [\humhub\modules\space\modules\manage\widgets\PendingApprovals::className(), ['space' => $space], ['sortOrder' => 20]],
+            [\humhub\modules\space\widgets\Members::className(), ['space' => $space], ['sortOrder' => 30]]
+        ]]);
         ?>
     </div>
     <script>
