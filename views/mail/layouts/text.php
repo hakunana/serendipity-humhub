f29ab4f7666b148a367d9415a54abad88779e8f1
SRMonti <stevemonti_@hotmail.com>
2017-07-21 19:00:21 +0200

    introduce mailtemplates
    start mailtemplates. noch changes from default so far. just a starting
    point...

---
+++ views/mail/layouts/text.php
@@ -0,0 +1,11 @@
+<?= $content; ?>
+
+
+---
+
+<?php if (isset(Yii::$app->view->params['showUnsubscribe']) && Yii::$app->view->params['showUnsubscribe'] === true) : ?>
+<?php $url = (isset(Yii::$app->view->params['unsubscribeUrl'])) ? Yii::$app->view->params['unsubscribeUrl'] : \yii\helpers\Url::to(['/notification/user'], true) ?>
+<?= Yii::t('base', 'Unsubscribe') ?>: <?= $url ?>
+<?php endif; ?>
+
+Powered by HumHub (http://www.humhub.org)
