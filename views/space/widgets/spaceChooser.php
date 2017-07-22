1d30aec92120e9c654032cc68a381a6f4873ef10
SRMonti <stevemonti_@hotmail.com>
2017-07-22 14:36:00 +0200

    Space Chooser update - löschen enterprise modul zwingend
    Update to new layout in navbar for spaces.
    funktioniert nur wenn das enterprise modul gelöscht ist, da sonst der
    controller vom enterprise modul vorrang hat...

---
+++ views/space/widgets/spaceChooser.php
@@ -0,0 +1,48 @@
+<?php
+/* @var $this \humhub\components\WebView */
+/* @var $currentSpace \humhub\modules\space\models\Space */
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+use humhub\modules\space\widgets\SpaceChooserItem;
+
+\humhub\modules\space\assets\SpaceChooserAsset::register($this);
+
+$noSpaceView = '<div class="no-space"><i class="fa fa-dot-circle-o"></i><br>' . Yii::t('SpaceModule.widgets_views_spaceChooser', 'My spaces') . '<b class="caret"></b></div>';
+
+$this->registerJsConfig('space.chooser', [
+    'noSpace' => $noSpaceView,
+    'remoteSearchUrl' =>  Url::to(['/space/browse/search-json']),
+    'text' => [
+        'info.remoteAtLeastInput' => Yii::t('SpaceModule.widgets_views_spaceChooser', 'To search for other spaces, type at least {count} characters.', ['count' => 2]),
+        'info.emptyOwnResult' => Yii::t('SpaceModule.widgets_views_spaceChooser', 'No member or following spaces found.'),
+        'info.emptyResult' => Yii::t('SpaceModule.widgets_views_spaceChooser', 'No result found for the given filter.'),
+    ],
+]);
+?>
+
+<li>
+    <ul>
+        <li class="divider"></li>
+        <li>
+            <ul class="media-list notLoaded" id="space-menu-spaces">
+                <?php foreach ($memberships as $membership): ?>
+                    <?= SpaceChooserItem::widget(['space' => $membership->space, 'updateCount' => $membership->countNewItems(), 'isMember' => true]); ?>
+                <?php endforeach; ?>
+                <?php foreach ($followSpaces as $followSpace): ?>
+                    <?= SpaceChooserItem::widget(['space' => $followSpace, 'isFollowing' => true]); ?>
+                <?php endforeach; ?>
+            </ul>
+        </li>
+
+        <?php if ($canCreateSpace): ?>
+            <li>
+                <div>
+                    <a href="#" class="btn btn-info col-md-12" data-action-click="ui.modal.load" data-action-url="<?= Url::to(['/space/create/create']) ?>">
+                        <?= Yii::t('SpaceModule.widgets_views_spaceChooser', 'Create new space') ?>
+                    </a>
+                </div>
+            </li>
+        <?php endif; ?>
+    </ul>
+</li>
---
+++ views/space/widgets/spaceChooserItem.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $space \humhub\modules\space\models\Space */
+
+use yii\helpers\Html;
+use humhub\libs\Helpers;
+?>
+
+<li<?= (!$visible) ? ' style="display:none"' : '' ?> data-space-chooser-item <?= $data ?> data-space-guid="<?= $space->guid; ?>" onmouseenter="$(this).find('#spacechooser_desc').show();" onmouseleave="$(this).find('#spacechooser_desc').hide();">
+    <a href="<?php echo $space->getUrl(); ?>">
+        <div id="spacechooser_parent" class="media">
+            <?=
+            \humhub\modules\space\widgets\Image::widget([
+                'space' => $space,
+                'width' => 24,
+                'htmlOptions' => [
+                    'class' => 'pull-left space-profile-acronym-1 space-acronym',
+            ]]);
+            ?>
+            <div class="media-body">
+                <strong class="space-name"><?php echo Html::encode($space->name); ?></strong>
+                <div  data-message-count="<?= $updateCount; ?>" class="badge badge-space messageCount pull-right tt" title="<?= Yii::t('SpaceModule.widgets_views_spaceChooserItem', '{n,plural,=1{# new entry} other{# new entries}} since your last visit', ['n' => $updateCount]); ?>"
+                    <?php if ($updateCount==0) {
+                        echo "style=\"display:none\"";
+                    }
+                    ?>>
+                <?= $updateCount; ?>
+                </div>
+                <div id="spacechooser_desc" style="display: none"
+                <br/>
+                    <p><?php echo Html::encode(Helpers::truncateText($space->description, 60)); ?></p>
+                </div>
+            </div>
+        </div>
+    </a>
+</li>
