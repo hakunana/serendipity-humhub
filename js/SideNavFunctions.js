4e0fe6e6a6d03e7ff35f02642976f79fe66204ee
SRMonti <stevemonti_@hotmail.com>
2017-07-22 16:10:05 +0200

    Bugfix of sort order spaces in navbar

--- js/SideNavFunctions.js
+++ js/SideNavFunctions.js
@@ -33,7 +33,7 @@ function reorder_spaces(){
     var $wrapper = $('#space-menu-spaces');
 
     $wrapper.find(".spacechooser_parent").sort(function(a, b) {
-        return a.getAttribute('update_count_value') + b.getAttribute('update_count_value');
+        return +(a.getAttribute('update_count_value')*-1) - +(b.getAttribute('update_count_value')*-1);
     }).appendTo($wrapper);
 
     //$spacelist = this.jQuery.find(".spacechooser_parent");
