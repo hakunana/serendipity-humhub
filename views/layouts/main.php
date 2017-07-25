1fd603298410d2135b047a1dcd6e228f2b778dd4
SRMonti <stevemonti_@hotmail.com>
2017-07-25 17:25:26 +0200

    Add comments and main description, fix delete issue

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -85,11 +85,40 @@
             $('#wallStream').masonry('layout');
         });
 
-        var element2 = $('.panel-body');
-        new ResizeSensor(element2, function() {
-            $('#wallStream').masonry('layout');
-        });
+        /*
+        TODO: Refactor code by implementing own module for correct masonry layout
+        A lot of issues are to be faced with current implementation of masonry.
+        The main issue is, that there are numerous actions which should trigger the re-layout of the masonry grid.
 
+        E.g.:
+        As described above, the open navigation action implicates a reload of the masonry layout.
+        Therefore a js library has been introduced, which is able to listen on resize of specific DIV.
+
+        One envisioned solution therefore, was to extend this listener, to listen on any resize-events of the actual wall entry panels.
+        Unfortunately the listener does not work. It is to be assumed, that the triggering does not work, as the wall entries are ActiveForm elements and thus AJAX-driven special components.
+
+        Code example as envisioned (following code snippet is not working):
+        ----------------------------------
+        var comment_element = $('.panel-body');
+        new ResizeSensor(comment_element, function() {
+        $('#wallStream').masonry('layout');
+        });
+        -----------------------------------
+
+        As the masonry layout is quite invasive, it requires subsequently a more subtle implementation via extra module.
+        This is described in the Javascript API (file protected/humhub/docs/guide/developer/javascript.md).
+
+        Idea: Create an own "masonry module" with additional action bindings and controller.
+        So for example the default action binding attribute data-action-change could be added to all elements in focus and used for triggerng the masonry grid reload layout function.
+
+        Example - add data-action-change="masonrymodule.reorder" to main wall-enttry panel div:
+        <div class="panel panel-default innerdivgrid col-lg-4 col-md-6 wall_[...] data-action-change="masonrymodule.reorder">
+
+        The js function reorder() in the module js resource would then call the $('#wallStream').masonry('layout') function of masonry.
+
+        Until then, there are just workarounds to solve various issues, e.g. via triggering the masonry-layout function onclick()
+           but delaying it for a couple of seconds so the page can first reload the section before the grid gets refreshed.
+         */
     </script>
     <!-- @Stefano End -->
 </html>
